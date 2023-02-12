6d2fcd2886758bea4a5dc46ec0c7c6baa53fb2ec
sintaeho <sth0701@icloud.com>
2023-02-12 18:53:56 +0900

    [feat] 로그인,메인 페이지 기초공사

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Auth from "./pages/Auth";
+import { lazy, Suspense } from "react";
+
+const Todo = lazy(() => import("./pages/Todo"));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Auth />} />
+        <Route
+          path="/todo"
+          element={
+            <Suspense fallback={<div>... 로딩중</div>}>
+              <Todo />
+            </Suspense>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Todo.js
@@ -0,0 +1,5 @@
+const Todo = () => {
+  return <div>Todo리스트 페이지</div>;
+};
+
+export default Todo;
